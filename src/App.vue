6bb01a4e1922be639566a8437127dea7de0cd60e
Coder_Q <1282117217@qq.com>
2022-07-20 05:23:56 +0800

    init: 🌈 新建 vue3 模板

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Test } from './components';
+</script>
+
+<template>
+  <div class="App">
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+      <a href="#" target="_blank">
+        <img src="/ts.svg" class="logo ts" alt="Ts logo" />
+      </a>
+    </div>
+    <h1>
+      <span class="vite">Vite + </span>
+      <span class="vue">Solid + </span>
+      <span class="ts">Ts</span>
+    </h1>
+    <div class="card">
+      <div class="title">练习黑板</div>
+      <div class="card-wrapper">
+        <Test />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import "./App.css";
+</style>
